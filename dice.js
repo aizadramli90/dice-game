715b04fdb114413a5eb78a1522525f30a93677ed
aizadramli90 <muhammadaizadr@gmail.com>
2024-02-04 21:54:39 +0800

    Created dice game

---
+++ dice.js
@@ -0,0 +1,31 @@
+function rollTheDice() {
+    let randomNumber_player1 = Math.floor(Math.random()*6) + 1;
+    let randomNumber_player2 = Math.floor(Math.random()*6) + 1;
+
+
+    document.getElementById("dice1").src = `dice${randomNumber_player1}`.png;
+    document.getElementById("dice2").src = `dice${randomNumber_player2}`.png;
+
+    let announce = document.querySelector("h1");
+
+if (randomNumber_player1 > randomNumber_player2) {
+    //winner = player1;
+    announce.innerHTML = "Player 1 Wins!";
+} else if (randomNumber_player1 < randomNumber_player2) {
+    //winner = player2;
+    announce.innerHTML = "Player 2 Wins!";
+} else {
+    announce.innerHTML = "It's a Draw!";
+
+}
+}
+
+
+
+
+
+
+
+
+
+
